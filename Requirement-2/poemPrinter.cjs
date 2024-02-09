c5d5418e0d4c9ba43384e53beaf3d88acb756db5
Ayush Agrawal <ayushagrawal1811@gmail.com>
2024-02-10 00:51:27 +0530

    Completed Requirement 2

---
+++ Requirement-2/poemPrinter.cjs
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+fs.readFile(`${__dirname}/poem.txt`, "utf8", (err, data) => {
+    if (err) {
+        console.error("Error reading the file:", err);
+        return;
+    }
+    console.log("File content:\n");
+    console.log(data);
+});
